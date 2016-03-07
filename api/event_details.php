8f5db32befc253e60c57bd3c693fae5cb80ab2d9
Shivanshu Goyal <shivanshu3@gmail.com>
2016-03-07 15:31:18 -0800

    event_details script complete

--- api/event_details.php
+++ api/event_details.php
@@ -1,3 +1,57 @@
 <?php
-    // Get details of an event
+
+/*
+Given an event id, it returns the following object:
+
+{
+    id: // the id you supplied
+    name:
+    start_time: // seconds since epoch
+    duration: // seconds
+    room_no:
+}
+
+The returned object will be empty - with no keys if an event with the
+given id does not exist.
+*/
+
+require 'database_connection.php';
+require 'utility.php';
+
+$id = $_GET['id'];
+
+// This will store the final result
+$json_result = array();
+
+// Get the event details
+$query = '
+SELECT Name, StartTime, Duration, RoomNo
+FROM Event
+WHERE Event.Eid = '.$id;
+
+$result = mysqli_query($link, $query);
+if ($result === FALSE) {
+    printf("query could not be executed.\n");
+    exit(1);
+} else {
+    // printf("query successfully executed.\n");
+}
+
+$all_rows = $result->fetch_all();
+
+// Event does not exist?
+if (count($all_rows) == 0) {
+    output_json($json_result);
+    exit(0);
+}
+
+// Constructing the json object:
+$json_result['id'] = $id;
+$json_result['name'] = $all_rows[0][0];
+$json_result['start_time'] = $all_rows[0][1];
+$json_result['duration'] = $all_rows[0][2];
+$json_result['room_no'] = $all_rows[0][3];
+
+output_json($json_result);
+
 ?>
