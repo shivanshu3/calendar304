15afcb134b095c0c544110e309c0902530dc8466
Shivanshu Goyal <shivanshu3@gmail.com>
2016-03-07 15:33:33 -0800

    Added a note in calendar_details saying that we might not need it.

--- api/calendar_details.php
+++ api/calendar_details.php
@@ -1,3 +1,8 @@
 <?php
-    // Get details of a calendar
+
+/**
+ * Get details of a calendar, given the calendar id.
+ * We might not need this, because user_details outputs the calendar info.
+ */
+
 ?>
