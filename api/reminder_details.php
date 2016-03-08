6925f5de4a7834782f1ff1344df2aa604d3665e2
Shivanshu Goyal <shivanshu3@gmail.com>
2016-03-07 17:19:13 -0800

    Added a note in reminder_details saying that it might not need to be implemented.

--- api/reminder_details.php
+++ api/reminder_details.php
@@ -1,3 +1,9 @@
 <?php
-    // Get details of a reminder
+
+/**
+ * Get details of a reminder, given the reminder id.
+ * We might not need this, because event_details outputs reminders
+ * associated with it.
+ */
+
 ?>
