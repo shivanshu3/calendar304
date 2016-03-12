bb10ff5ec07034b001d67c99ba94c9d2bb2499d2
Shivanshu Goyal <shivanshu3@gmail.com>
2016-03-11 18:00:54 -0800

    - Added some documentation. - Added a callback function which runs when the window hash changes.

--- frontend/UIManager.js
+++ frontend/UIManager.js
@@ -1,15 +1,40 @@
+/**
+ * UIManager is a singleton class which initializes the app.
+ * It is a singleton class.
+ */
+
 var UIManager = function() {
 };
 
 UIManager.singletonInstance = null;
 
+/**
+ * Returns the singleton instance of this class.
+ */
 UIManager.getInstance = function() {
     if (UIManager.singletonInstance == null) {
         UIManager.singletonInstance = new UIManager();
     }
     return UIManager.singletonInstance;
-}
+};
 
+/**
+ * Runs when the app loads.
+ */
 UIManager.prototype.init = function() {
+    var _this = this;
     console.log("Initializing!");
-}
+
+    // Register a callback for when the hash changes:
+    $(window).on('hashchange', function() {
+        _this.hashChanged(location.hash.slice(1));
+    });
+};
+
+/**
+ * Runs when the hash changes.
+ * The argument passed to this function is the new hash.
+ */
+UIManager.prototype.hashChanged = function(hash) {
+    console.log("New Hash: " + hash);
+};
