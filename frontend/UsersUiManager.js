7ede471d7d440ed51ad59edce57095432402679a
Shivanshu Goyal <shivanshu3@gmail.com>
2016-03-13 11:45:37 -0700

    Added callback functions for sign in button and sign up button.

--- frontend/UsersUiManager.js
+++ frontend/UsersUiManager.js
@@ -22,4 +22,29 @@ UsersUiManager.getInstance = function() {
  * Runs when the app loads.
  */
 UsersUiManager.prototype.init = function() {
+    var _this = this;
+
+    // Register callbacks for the 2 buttons:
+    signInButton = $('#sign_in_box button');
+    signInButton.click(function() {
+        _this.signInButtonClicked();
+    });
+    signUpButton = $('#sign_up_box button');
+    signUpButton.click(function() {
+        _this.signUpButtonClicked();
+    });
+};
+
+/**
+ * Runs when sign in button is clicked.
+ */
+UsersUiManager.prototype.signInButtonClicked = function() {
+    console.log('sign in');
+};
+
+/**
+ * Runs when sign up button is clicked.
+ */
+UsersUiManager.prototype.signUpButtonClicked = function() {
+    console.log('sign up');
 };
