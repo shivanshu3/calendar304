22959de316722400585ba87b17d40b13000496b9
Shivanshu Goyal <shivanshu3@gmail.com>
2016-03-18 22:31:39 -0700

    Started working on event_edit.php

--- api/event_edit.php
+++ api/event_edit.php
@@ -1,3 +1,26 @@
 <?php
-    // Edit details of an event
+
+/*
+Used to edit the following fields of an event:
+
+{
+    name:
+    start_time: // seconds since epoch
+    duration: // seconds
+    location: {
+        room_no:
+        max_occ:
+    }
+}
+
+You need to give the event id so we know which event to edit.
+You need to provide new values of start_time, duration, and room_no.
+
+The returned object looks like the following:
+
+{
+    success: true/false
+}
+*/
+
 ?>
