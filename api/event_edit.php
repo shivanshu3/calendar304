a15e22a66f7c556905c680a94f5f773a3795e857
Shivanshu Goyal <shivanshu3@gmail.com>
2016-03-20 19:16:47 -0700

    Implemented the event_edit script, haven't tested it yet.

--- api/event_edit.php
+++ api/event_edit.php
@@ -14,7 +14,7 @@ Used to edit the following fields of an event:
 }
 
 You need to give the event id so we know which event to edit.
-You need to provide new values of start_time, duration, and room_no.
+You need to provide new values of name, start_time, duration, and room_no.
 
 The returned object looks like the following:
 
@@ -22,5 +22,32 @@ The returned object looks like the following:
     success: true/false
 }
 */
+
+require 'database_connection.php';
+require 'utility.php';
+
+$event_id = $_GET['event_id'];
+$name = $_GET['name'];
+$start_time = $_GET['start_time'];
+$duration = $_GET['duration'];
+$room_no = $_GET['room_no'];
+
+// This will store the final result:
+$json_result = array();
+
+$query = "
+UPDATE Event
+SET Name = '$name', StartTime = '$start_time', Duration = '$duration',
+RoomNo = '$room_no'
+WHERE Eid = '$event_id'";
+
+$result = mysqli_query($link, $query);
+if ($result === FALSE) {
+    printf("query could not be executed.\n");
+    exit(1);
+}
+
+$json_result['success'] = true;
+output_json($json_result);
 
 ?>
