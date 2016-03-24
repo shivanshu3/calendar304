6092beb272e618378e4a6f9c8d5bbfd792faa31d
Jeff Coukell <jeffcoukell@gmail.com>
2016-03-24 12:18:51 -0700

    Implemented calendar_delete and calendar_edit queries

--- api/calendar_delete.php
+++ api/calendar_delete.php
@@ -1,3 +1,41 @@
 <?php
-    // Delete a calendar
+
+/* Delete a calendar, given its calendar id
+ * 
+ * The returned object looks like the following:
+ * 
+ * {
+ *     success: true/false
+ * }
+ */
+
+require 'database_connection.php';
+require 'utility.php';
+
+$calendar_id = $_GET['calendar_id'];
+
+// This will store the final result:
+$json_result = array();
+
+// Delete all records from the Contains table that match calendar_id
+$delete_calendar_query = "DELETE FROM `Contains` WHERE `Cid` = $calendar_id";
+
+$result = mysqli_query($link, $delete_calendar_query);
+if ($result === FALSE) {
+	printf("query 1 could not be executed.\n");
+	exit(1);
+}
+
+// Delete the record from the Calendar table
+$delete_calendar_query = "DELETE FROM `Calendar` WHERE `Cid` = $calendar_id";
+
+$result = mysqli_query($link, $delete_calendar_query);
+if ($result === FALSE) {
+	printf("query 2 could not be executed.\n");
+	exit(1);
+}
+
+$json_result['success'] = true;
+output_json($json_result);
+
 ?>
--- api/calendar_edit.php
+++ api/calendar_edit.php
@@ -1,3 +1,35 @@
 <?php
-    // Change name of a calendar
+
+/*
+ * Edits the name of a calendar, given its calendar id and a new name
+ *
+ * The returned object looks like the following:
+ * {
+ *     success: true/false
+ * }
+ */
+
+require 'database_connection.php';
+require 'utility.php';
+
+$calendar_id = $_GET['calendar_id'];
+$name = $_GET['name'];
+
+// This will store the final result
+$json_result = array();
+
+$calendar_edit_query = "
+UPDATE `Calendar`
+SET `Name` = '$name'
+WHERE `Cid` = $calendar_id";
+
+$result = mysqli_query($link, $calendar_edit_query);
+if ($result === FALSE) {
+	printf("query could not be executed.\n");
+	exit(1);
+}
+
+$json_result['success'] = true;
+output_json($json_result);
+
 ?>
