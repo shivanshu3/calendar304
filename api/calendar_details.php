9c2ae200c35aa435f8f9e89622e4fcb81b33d625
Shivanshu Goyal <shivanshu3@gmail.com>
2016-03-25 18:51:26 -0700

    Implemented calendar_details.php

--- api/calendar_details.php
+++ api/calendar_details.php
@@ -1,8 +1,50 @@
 <?php
 
-/**
- * Get details of a calendar, given the calendar id.
- * We might not need this, because user_details outputs the calendar info.
- */
+/*
+Given a calendar id, it returns the following object:
+
+{
+    id: // the id you supplied
+    name: // the full name of this user
+    user_id: // the id of the user who owns this calendar
+}
+
+The returned object will be empty - with no keys if a calendar with the
+given id does not exist.
+*/
+
+require 'database_connection.php';
+require 'utility.php';
+
+$id = $_GET['id'];
+
+// This will store the final result
+$json_result = array();
+
+// Get the calendar details:
+$query = "
+SELECT DISTINCT *
+FROM Calendar
+WHERE Cid = $id";
+
+$result = mysqli_query($link, $query);
+if ($result === FALSE) {
+    printf("query could not be executed.\n");
+    exit(1);
+}
+
+$all_rows = $result->fetch_all();
+
+// Calendar does not exist?
+if (count($all_rows) == 0) {
+    output_json($json_result);
+    exit(0);
+}
+
+$json_result['id'] = $id;
+$json_result['name'] = $all_rows[0][1];
+$json_result['user_id'] = $all_rows[0][2];
+
+output_json($json_result);
 
 ?>
